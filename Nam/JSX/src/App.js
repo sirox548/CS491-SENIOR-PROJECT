a7ba54a978a1013fb330bd1920ef0170b47ec253
ntb6 <56608716+ntb6@users.noreply.github.com>
2020-03-03 19:45:03 -0500

    Create App.js

---
+++ Nam/JSX/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "normalize.css";
+import "./App.css";
+
+import FormikForm from "./FormikForm";
+
+export default function App() {
+  return (
+    <>
+      <FormikForm />
+    </>
+  );
+}
